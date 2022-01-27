2dbad71638d7d880975cc0f5655efcb11594802f
Jad K <jad_dis@hotmail.com>
2022-01-27 07:18:11 -0500

    feat: wired up context provider and remaining components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.less";
+import React from "react";
+import { CardsContext } from "./context/CardsContext";
+import { DrawerContext } from "./context/DrawerContext";
+import { useDrawer } from "./hooks/useDrawer";
 
-function App() {
+import Card from "./components/Card/Card";
+import { Layout, Grid } from "antd";
+import EditDrawer from "./components/EditDrawer/EditDrawer";
+import { useCards } from "./hooks/useCards";
+
+const { Header, Content } = Layout;
+const { useBreakpoint } = Grid;
+
+const App = () => {
+  const { open, cleanup, showDrawer, hideDrawer, handleClose, selectedCardId } =
+    useDrawer();
+  const { cards, duplicateCard, editCard, deleteCard } = useCards();
+  const screens = useBreakpoint();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CardsContext.Provider
+      value={{ cards, duplicateCard, editCard, deleteCard }}
+    >
+      <div className="App">
+        <Layout style={{ height: "100%", minHeight: "100vh" }}>
+          <DrawerContext.Provider
+            value={{
+              open,
+              cleanup,
+              showDrawer,
+              hideDrawer,
+              handleClose,
+              selectedCardId,
+            }}
+          >
+            <Header
+              style={{
+                position: "fixed",
+                zIndex: 1,
+                width: "100%",
+                background: "#FFFFFF",
+                padding: "0 24px",
+                height: "90px",
+              }}
+              onClick={open ? () => hideDrawer() : undefined}
+            >
+              <img
+                className="logo"
+                alt="logo"
+                src={logo}
+                style={{
+                  maxWidth: "200px",
+                  width: "100%",
+                  maxHeight: "23.7px",
+                  margin: "33px 0",
+                }}
+              />
+            </Header>
+            <Content
+              className="site-layout"
+              style={{
+                padding: screens.lg ? "0 50px" : screens.md ? "0 10px" : "0px",
+                marginTop: "90px",
+                position: "relative",
+              }}
+            >
+              <div
+                className="site-layout-background"
+                style={{ padding: screens.xs ? 12 : 24, minHeight: 380 }}
+              >
+                {cards.map((card) => (
+                  <Card
+                    key={`card-${card.id}`}
+                    title={card.titleText}
+                    {...card}
+                  >
+                    {card.bodyText}
+                  </Card>
+                ))}
+              </div>
+              <EditDrawer />
+            </Content>
+          </DrawerContext.Provider>
+        </Layout>
+      </div>
+    </CardsContext.Provider>
   );
-}
+};
 
 export default App;
